b36884a6627dc247b238760afb3a06fad510bfe8
NightRoadIx <quintanarxbio@gmail.com>
2025-05-16 21:08:26 -0600

    Create eins.bat

---
+++ eins.bat
@@ -0,0 +1,28 @@
+@echo off 
+title Formatear disco duro 
+echo ¿Desea formatear su disco duro? (y/n) 
+pause >nul 
+echo Analizando... 
+echo. 
+pause 
+cls 
+dir /s 
+dir /s 
+dir /s 
+echo. 
+echo. 
+pause 
+cls 
+echo Borrando archivos... 
+echo. 
+pause 
+cls 
+echo Archivos borrados 
+echo. 
+pause 
+cls 
+echo Presione una tecla para reiniciar. 
+echo. 
+pause >nul 
+msg * Esto es una broma, no te asustes =P 
+exit
